2105e2922cc344307ec01d54bc902ff85a7c92f0
Yuriy Nabasov <nabasoff@gmail.com>
2025-06-28 21:56:28 +0300

    add-load-more-btn

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Logo from "./components/Logo/Logo.jsx";
 import LearnMoreBtn from "./components/LearnMoreBtn/LearnMoreBtn.jsx";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Logo />
       <LearnMoreBtn />
+      <LoadMoreBtn onClick={() => console.log("Load more clicked")} />
       {/* <AppRoutes /> */}
       <Toaster position="top-right" />
     </BrowserRouter>
