01019c8929ad7fd79b1115f29a3b4423c7209a95
Yuriy Nabasov <nabasoff@gmail.com>
2025-06-28 21:12:29 +0300

    add-learn-more-btn

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 // import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
 import Logo from "./components/Logo/Logo.jsx";
+import LearnMoreBtn from "./components/LearnMoreBtn/LearnMoreBtn.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Logo />
+      <LearnMoreBtn />
       {/* <AppRoutes /> */}
       <Toaster position="top-right" />
     </BrowserRouter>
