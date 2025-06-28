6be24a131f9f08e99b5051d2b20e21461d8ed3de
Yuriy Nabasov <nabasoff@gmail.com>
2025-06-28 23:54:35 +0300

    add-favorite-btn

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Toaster } from "react-hot-toast";
 import Logo from "./components/Logo/Logo.jsx";
 import LearnMoreBtn from "./components/LearnMoreBtn/LearnMoreBtn.jsx";
 import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn.jsx";
+import FavoriteBtn from "./components/FavoriteBtn/FavoriteBtn.jsx";
 
 const App = () => {
   return (
@@ -11,6 +12,13 @@ const App = () => {
       <Logo />
       <LearnMoreBtn />
       <LoadMoreBtn onClick={() => console.log("Load more clicked")} />
+      <div style={{ padding: "40px", textAlign: "center" }}>
+        <h2>Test Favorite Button</h2>
+        <FavoriteBtn
+          recipeId="6462a8f74c3d0ddd2889802b"
+          isInitiallyFavorite={false}
+        />
+      </div>
       {/* <AppRoutes /> */}
       <Toaster position="top-right" />
     </BrowserRouter>
