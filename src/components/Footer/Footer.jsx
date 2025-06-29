49a105bf8dfe023ea34d7b65891daddc244e6311
Anton Kostiuk <antkos90@gmail.com>
2025-06-29 10:11:31 +0200

    footer_01

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,34 @@
-// import css from "./Footer.module.css";
+import { useLocation } from "react-router-dom";
+import css from "./Footer.module.css";
 
 const Footer = () => {
+  const location = useLocation();
+  const isRegisterPage = location.pathname === "/profile";
   return (
-    <>
-      <p>Footer</p>
-    </>
+    <footer className={css.footer}>
+      <div className={css.container}>
+        <div className={css.footer__logo}>
+          <a href="/">
+            <img
+              src="/images/Logo.png"
+              alt="Logo.png"
+              className={css.logo_img}
+            />
+          </a>
+        </div>
+
+        <div className={css.footer_copyright}>
+          <p>
+            © {new Date().getFullYear()} CookingCompanion. All rights reserved.
+          </p>
+        </div>
+
+        <nav className={css.footer_nav}>
+          <a href="/">Recipes</a>
+          {!isRegisterPage && <a href="/profile">Account</a>}
+        </nav>
+      </div>
+    </footer>
   );
 };
 
