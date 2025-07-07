9b850d699fae826bb14f6252f8ff76c97010eb8f
Ваше ім'я < email>
2025-07-07 20:21:13 +0300

    conponent NotFound and RecipeViewPage

--- src/pages/RecipeViewPage/RecipeViewPage.jsx
+++ src/pages/RecipeViewPage/RecipeViewPage.jsx
@@ -1,13 +1,20 @@
 // import css from './RecipeViewPage.module.css';
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import NotFound from "../../components/NotFound/NotFoundRecipe/NotFoundRecipe.jsx";
+import { selectRecipeById } from "../../redux/recipes/selectors.js";
 import RecipeDetails from "../../components/RecipeDetails/RecipeDetails.jsx";
 
 const RecipeViewPage = () => {
-  return (
-    <>
-      {/* <p>RecipeViewPage</p> */}
-      <RecipeDetails />
-    </>
-  );
+  const { id } = useParams();
+  const recipe = useSelector(selectRecipeById(id));
+
+  if (!recipe) {
+    return <NotFound />;
+  }
+
+  return <RecipeDetails recipe={recipe} />;
 };
 
 export default RecipeViewPage;
