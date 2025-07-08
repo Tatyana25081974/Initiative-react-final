1c8a10ef419e9d334cfb2ff9fe9c08790680be72
Ваше ім'я < email>
2025-07-08 21:42:27 +0300

    style: Information about wrong recipe ID, and change color & border in NetworkError component

--- src/components/NotFound/NotFoundRecipe/NotFoundRecipe.jsx
+++ src/components/NotFound/NotFoundRecipe/NotFoundRecipe.jsx
@@ -3,11 +3,13 @@ import css from "./NotFoundRecipe.module.css";
 export default function NotFound() {
   return (
     <div className={css.wrapper}>
-      <h2 className={css.title}>404 — Not Found</h2>
-      <p className={css.text}>
-        Sorry, we couldn’t find the recipe you’re looking for. It may have been
-        deleted or moved.
-      </p>
+      <div className={css.errorBox}>
+        <h2 className={css.title}>404 — Not Found</h2>
+        <p className={css.text}>
+          Sorry, we couldn’t find the recipe you’re looking for. It may have
+          been deleted or moved.
+        </p>
+      </div>
     </div>
   );
 }
