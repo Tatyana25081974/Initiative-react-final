7448a6bbfecf1190ee824cc4b90fdcf64a5f2016
Ваше ім'я < email>
2025-07-08 22:13:20 +0300

    change text error page

--- src/components/NotFound/NotFoundRecipe/NotFoundRecipe.jsx
+++ src/components/NotFound/NotFoundRecipe/NotFoundRecipe.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className={css.wrapper}>
       <div className={css.errorBox}>
-        <h2 className={css.title}>404 — Not Found</h2>
+        <h2 className={css.title}>We couldn't find that recipe</h2>
         <p className={css.text}>
           Sorry, we couldn’t find the recipe you’re looking for. It may have
           been deleted or moved.
