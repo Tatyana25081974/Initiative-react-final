8a2da2f0b8733ce7a4251899757d032d3299c841
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2025-07-10 17:45:39 +0200

    clear current recipe

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -22,6 +22,7 @@ import { selectIngredients } from "../../redux/filters/selectors.js";
 import { firstLetterToUpperCase } from "../../utils/firstLetterToUpperCase.js";
 import toast from "react-hot-toast";
 import css from "./RecipeDetails.module.css";
+import { selectIsGlobalLoading } from "../../redux/isGlobalLoading.js";
 
 export default function RecipeDetails() {
   const dispatch = useDispatch();
@@ -102,90 +103,93 @@ export default function RecipeDetails() {
 
   return (
     <Container>
-      <div className={css.container}>
-        <div className={css.headerSection}>
-          <div className={css.imageWrapper}>
-            <img className={css.image} src={thumb} alt={title} />
+      {selectIsGlobalLoading && (
+        <div className={css.container}>
+          <div className={css.headerSection}>
+            <div className={css.imageWrapper}>
+              <img className={css.image} src={thumb} alt={title} />
+            </div>
+            <h2 className={css.title}>{firstLetterToUpperCase(title)}</h2>
           </div>
-          <h2 className={css.title}>{firstLetterToUpperCase(title)}</h2>
+
+          <div className={css.content}>
+            <div className={css.generalInfoBlock}>
+              <div className={`${css.block} ${css.highlightedBlock}`}>
+                <h4>General information</h4>
+                <p>
+                  <strong>Category:</strong> {category}
+                </p>
+                <p>
+                  <strong>Cooking time:</strong> {time} minutes
+                </p>
+                <p>
+                  <strong>Caloric content:</strong>{" "}
+                  {cals ? `~${cals} kcal` : "—"}
+                </p>
+              </div>
+
+              <button
+                className={css.saveButton}
+                onClick={(e) => {
+                  e.currentTarget.blur(); // прибиратє фокус після кліку
+                  favorite
+                    ? handleClickDeleteFavorite()
+                    : handleClickAddFavorite();
+                }}
+              >
+                {isLoading ? (
+                  <BarLoader className={css.loader} />
+                ) : (
+                  <>
+                    {favorite ? "Remove" : "Save"}
+                    <FaRegBookmark />
+                  </>
+                )}
+              </button>
+            </div>
+
+            <div className={css.otherInfoBlock}>
+              <div className={css.block}>
+                <h3>About recipe</h3>
+                <p>{firstLetterToUpperCase(description)}</p>
+              </div>
+
+              <div className={css.block}>
+                <h3>Ingredients</h3>
+                <ul className={css.ingredientsList}>
+                  {ingredients.map(({ id, measure }) => {
+                    const ingredient = ingredientList.find(
+                      (item) => item._id === id
+                    );
+                    return (
+                      <li key={id}>
+                        {ingredient ? ingredient.name : "Unknown ingredient"} -
+                        {measure}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+
+              <div className={css.block}>
+                <h3>Preparation Steps:</h3>
+                <ul className={css.instructionsList}>
+                  {instructions
+                    .split("\n")
+                    .filter((step) => step.trim() !== "")
+                    .map((step, index) => (
+                      <li key={index} className={css.instructionItem}>
+                        {firstLetterToUpperCase(step)}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <SaveAuthModal isOpen={isModalOpen} onRequestClose={closeModal} />
         </div>
-
-        <div className={css.content}>
-          <div className={css.generalInfoBlock}>
-            <div className={`${css.block} ${css.highlightedBlock}`}>
-              <h4>General information</h4>
-              <p>
-                <strong>Category:</strong> {category}
-              </p>
-              <p>
-                <strong>Cooking time:</strong> {time} minutes
-              </p>
-              <p>
-                <strong>Caloric content:</strong> {cals ? `~${cals} kcal` : "—"}
-              </p>
-            </div>
-
-            <button
-              className={css.saveButton}
-              onClick={(e) => {
-                e.currentTarget.blur(); // прибиратє фокус після кліку
-                favorite
-                  ? handleClickDeleteFavorite()
-                  : handleClickAddFavorite();
-              }}
-            >
-              {isLoading ? (
-                <BarLoader className={css.loader} />
-              ) : (
-                <>
-                  {favorite ? "Remove" : "Save"}
-                  <FaRegBookmark />
-                </>
-              )}
-            </button>
-          </div>
-
-          <div className={css.otherInfoBlock}>
-            <div className={css.block}>
-              <h3>About recipe</h3>
-              <p>{firstLetterToUpperCase(description)}</p>
-            </div>
-
-            <div className={css.block}>
-              <h3>Ingredients</h3>
-              <ul className={css.ingredientsList}>
-                {ingredients.map(({ id, measure }) => {
-                  const ingredient = ingredientList.find(
-                    (item) => item._id === id
-                  );
-                  return (
-                    <li key={id}>
-                      {ingredient ? ingredient.name : "Unknown ingredient"} -
-                      {measure}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-
-            <div className={css.block}>
-              <h3>Preparation Steps:</h3>
-              <ul className={css.instructionsList}>
-                {instructions
-                  .split("\n")
-                  .filter((step) => step.trim() !== "")
-                  .map((step, index) => (
-                    <li key={index} className={css.instructionItem}>
-                      {firstLetterToUpperCase(step)}
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <SaveAuthModal isOpen={isModalOpen} onRequestClose={closeModal} />
-      </div>
+      )}
     </Container>
   );
 }
